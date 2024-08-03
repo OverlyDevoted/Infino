a7eba300f04fa6f08493b1652a644a7694435be7
OverlyDevoted <58229165+OverlyDevoted@users.noreply.github.com>
2024-08-03 17:23:09 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import { FLICKR_API_KEY } from "./constants/flickr.constants";
+
+function App() {
+  console.log(FLICKR_API_KEY);
+
+  return <>Hello Vinted!</>;
+}
+
+export default App;
